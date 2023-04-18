cd04a758fedd9884e19aad7444558a213a6acc4d
Henrique Soares <101074262+GyokuroKunso@users.noreply.github.com>
2023-04-18 11:28:21 -0300

    Update Tabela .php

--- Tabela .php
+++ Tabela .php
@@ -1,5 +1,5 @@
-CREATE DATABASE uc_programacao;
-USE uc_programacao;
+CREATE DATABASE henrique;
+USE henrique;
 
 CREATE TABLE curso (
 cod_curso INTEGER,
@@ -68,7 +68,7 @@ VALUES
      (1, "Ciência da computação", "CCO"),
      (2, "Analise de Desenvolvimento de Sistema","ADS"),
      (3, "Jogos Digitais","JDI"),
-	  (4, "Gestão de Tecnologia de Informação","GTI"),
+     (4, "Gestão de Tecnologia de Informação","GTI"),
      (5, "Sistema de Informação","STI");
      
 INSERT INTO disciplina (cod_disciplina, nome_disciplina)
@@ -106,3 +106,84 @@ VALUES
       (10,"Noite",4),
       (11,"Noite",5),
       (12,"Noite",6);
+      
+INSERT INTO curso_disciplina
+(cod_curso_disciplina, curso, disciplina)
+VALUES
+      (1,1,1),
+      (2,1,2),
+      (3,1,3),
+      (4,1,4),
+      (5,1,5),
+      (6,1,6),
+      (7,2,1),
+      (8,2,2),
+      (9,2,3),
+      (10,2,4),
+      (11,2,5),
+      (12,2,6),
+      (13,3,1),
+      (14,3,2),
+      (15,3,3),
+      (16,3,4),
+      (17,3,5),
+      (18,3,6),
+      (19,4,1),
+      (20,4,2),
+      (21,4,3),
+      (22,4,4),
+      (23,4,5),
+      (24,4,6),
+      (25,5,1),
+      (26,5,2),
+      (27,5,3),
+      (28,5,4),
+      (29,5,5),
+      (30,5,6);
+		
+		
+INSERT INTO aluno_curso (cod_aluno_curso, aluno, curso)
+VALUES
+      (1,'202300001',1),
+      (2,'202300002',2),
+      (3,'202300003',3),
+      (4,'202300004',4),
+      (5,'202300005',5),
+      (6,'202300006',1),
+      (7,'202300007',2),
+      (8,'202300008',3),
+      (9,'202300009',4);
+
+INSERT INTO aluno_turma (cod_aluno_turma, aluno, turma)
+VALUES
+      (1,'202300001',1),
+      (2,'202300001',2),
+      (3,'202300002',1),
+      (4,'202300002',2),
+      (5,'202300003',1),
+      (6,'202300003',2),
+      (7,'202300004',1),
+      (8,'202300004',2),
+      (9,'202300005',1),
+      (10,'202300005',2),
+      (11,'202300006',9),
+      (12,'202300006',10),
+      (13,'202300007',9),
+      (14,'202300007',10),
+      (15,'202300008',9),
+      (16,'202300008',10),
+      (17,'202300009',9),
+      (18,'202300009',10);
+      
+      
+SELECT matricula,nome,cpf FROM aluno
+WHERE matricula = "202300006"
+WHERE nome = "Steve Rogers"
+WHERE cpf = "6666666-66"
+
+
+SELECT * FROM curso , aluno
+WHERE sigla = "JDI" OR sigla = "CCO" 
+AND cpf = "111111111-11"
+
+SELECT * FROM aluno_curso ,aluno WHERE matricula = "202300001"
