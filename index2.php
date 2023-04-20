560219503c3b669cd64ac9ed2480889ddbee895f
Henrique Soares <101074262+GyokuroKunso@users.noreply.github.com>
2023-04-20 11:39:11 -0300

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,45 @@
+<?php
+
+$variavel = 10;
+
+if ($variavel <20) {
+    echo "É menor que 20";
+} else {
+    echo "É maior que 20";
+}
+
+echo "<br><br>";
+
+for($i =0; $i<= 10; $i++){
+    echo $i." ";
+}
+
+echo"<br><br>";
+echo"Usando While<br>";
+echo" The World is Infinite<br>";
+
+
+$j= 0;
+
+while ($j <= 10) {
+    echo $j."";
+    $j++;
+}
+
+echo "<br><br>";
+echo "Utilizando Switch...case<br>";
+
+$switch = 1;
+switch($switch) {
+    case 1: echo "Número 1";
+      break;
+    default: echo "Não é o valor informado";
+      break;
+
+}
+
+
+
+
+
+?>
